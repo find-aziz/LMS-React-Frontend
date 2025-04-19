d8f8a2a164889fe375ce1a86427a92ffb0cbcdf0
Aziz Rehman Sardar <sardaraziz1239@gmail.com>
2025-04-19 21:41:58 +0530

    proper setup and folder structure with homepage

---
+++ src/pages/educator/StudentsEnrolled.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentsEnrolled = () => {
+  return <div>StudentsEnroll</div>;
+};
+
+export default StudentsEnrolled;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontSize: {
+        "course-details-heading-small": ["26px", { lineHeight: "36px" }],
+        "course-details-heading-large": ["36px", { lineHeight: "44px" }],
+        "home-heading-small": ["28px", { lineHeight: "34px" }],
+        "home-heading-large": ["48px", { lineHeight: "56px" }],
+        default: ["15px", { lineHeight: "21px" }],
+      },
+      gridTemplateColumns: {
+        "auto": "repeat(auto-fit, minmax(200px, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
