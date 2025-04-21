4e500e69406e51a1f84ded3ed2067acdba838530
Aziz Rehman Sardar <sardaraziz1239@gmail.com>
2025-04-21 11:28:18 +0530

    educator's navbar sidebar and footer

--- src/components/educator/Navbar.jsx
+++ src/components/educator/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { assets, dummyEducatorData as educatorData } from "../../assets/assets";
+import { UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { user } = useUser();
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3">
+      <Link to={"/"}>
+        <img src={assets.logo} className="w-28 lg:w-32" alt="logo" />
+      </Link>
+      <div className="flex items-center gap-5 text-gray-500 relative">
+        <p>Hi! {user ? user.fullName : "Developers"}</p>
+        {user ? (
+          <UserButton />
+        ) : (
+          <img
+            src={assets.profile_img}
+            className="max-w-8 rounded-full"
+            alt="user"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/student/Footer.jsx
+++ src/components/student/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <p className="py-4 text-center text-xs md:text-sm text-white/60">Copyright 2024 © Edemy. All Right Reserved.</p>
+      <p className="py-4 text-center text-xs md:text-sm text-white/60">Copyright 2025 © Edemy. All Right Reserved.</p>
     </footer>
   );
 };
